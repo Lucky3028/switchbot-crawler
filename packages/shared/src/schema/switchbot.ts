e6aa2dbfb3a487ec9d92eb1a0a380e264ef707f7
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-08-19 12:13:19 +0900

    fix: fix extend zod

--- packages/shared/src/schema/switchbot.ts
+++ packages/shared/src/schema/switchbot.ts
@@ -1,25 +1,18 @@
-import { type ZodRawShape, z } from 'zod';
+import { z } from 'zod';
 
 /**
- * SwitchBotのAPIからのHTTPレスポンススキーマ
- * @param bodyObject ボディのスキーマ
- * @returns HTTPレスポンススキーマ
+ * SwitchBotのAPIからのHTTPレスポンススキーマ（bodyは含まれない）
  */
-const generateResponseSchema = (bodyObject: ZodRawShape) =>
-  z.object({
-    /**
-     * HTTPステータスコード
-     */
-    statusCode: z.number(),
-    /**
-     * メッセージ
-     */
-    message: z.string(),
-    /**
-     * ボディ
-     */
-    body: z.object(bodyObject),
-  });
+const baseSchema = z.object({
+  /**
+   * HTTPステータスコード
+   */
+  statusCode: z.number(),
+  /**
+   * メッセージ
+   */
+  message: z.string(),
+});
 
 /**
  * 温度計のステータスを表すスキーマ
@@ -42,7 +35,7 @@ const meterStatus = {
 /**
  * 温度計のステータスを示すレスポンスのスキーマ
  */
-export const meterStatusResponse = generateResponseSchema(meterStatus);
+export const meterStatusResponse = baseSchema.extend({ body: z.object(meterStatus) });
 
 /**
  * エアコンの設定を変更するコマンドのスキーマ
@@ -70,4 +63,4 @@ export type AirConditionerCommand = z.infer<typeof airConditionerCommand>;
 /**
  * エアコンの設定を変更するコマンドを実行したあとに、APIから返ってくるHTTPレスポンスのスキーマ
  */
-export const controlCommandResponse = generateResponseSchema({});
+export const controlCommandResponse = baseSchema;
