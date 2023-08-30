74a60685cce470d70fe6b1327b8970affdb563eb
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-08-30 23:33:09 +0900

    feat: add isProduction

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,4 @@
 export { initSentry } from './sentry';
 export { switchbot } from './switchbot';
 export * from './error';
+export { isProduction } from './stage';
---
+++ packages/shared/src/stage.ts
@@ -0,0 +1,1 @@
+export const isProduction = (stage: NodeEnv) => stage === 'production';
