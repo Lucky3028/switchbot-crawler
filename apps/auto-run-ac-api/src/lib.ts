b363e62cdeca88d05cc29c1c53842c912c7ba93b
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-09-08 19:16:44 +0900

    refactor: make arg name proper

--- apps/auto-run-ac-api/src/lib.ts
+++ apps/auto-run-ac-api/src/lib.ts
@@ -1,6 +1,6 @@
 /**
  * 文字列を10進数の整数にパースする
- * @param s 文字列
+ * @param str 文字列
  * @returns 10進数の整数
  */
-export const parseDecimalInt = (s: string) => parseInt(s, 10);
+export const parseDecimalInt = (str: string) => parseInt(str, 10);
