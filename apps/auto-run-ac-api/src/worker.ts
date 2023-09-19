4ccbd5eca6302416b44d4290740f84c73f6c13dd
Lucky3028 <me@mail.cap-l.cc>
2023-09-20 00:21:58 +0900

    fix: remove /doc

--- apps/auto-run-ac-api/src/worker.ts
+++ apps/auto-run-ac-api/src/worker.ts
@@ -3,14 +3,6 @@ import { defaultTriggersApi, triggersApi } from './api';
 
 const app = new OpenAPIHono();
 
-app.doc('/doc', {
-  openapi: '3.1.0',
-  info: {
-    version: '1.0.0',
-    title: 'auto-run-ac API',
-  },
-});
-
 const route = app
   .get('/', (c) => c.text('Hello Hono!'))
   .route('/defaultTriggers', defaultTriggersApi)
