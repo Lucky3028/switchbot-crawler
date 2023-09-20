c2a9d94f00977fcb350627d14d6a30f16004f6ce
Lucky3028 <me@mail.cap-l.cc>
2023-09-20 23:31:45 +0900

    feat: add nanoid fun

--- apps/auto-run-ac-api/src/lib.ts
+++ apps/auto-run-ac-api/src/lib.ts
@@ -1,6 +1,15 @@
+import { customAlphabet } from 'nanoid';
+import { alphanumeric } from 'nanoid-dictionary';
+
 /**
  * 文字列を10進数の整数にパースする
  * @param str 文字列
  * @returns 10進数の整数
  */
 export const parseDecimalInt = (str: string) => parseInt(str, 10);
+
+/**
+ * 英数字からなるnanoidを生成する
+ * @returns 英数字からなるnanoid。英字には大文字小文字いずれも含まれる
+ */
+export const nanoid = () => customAlphabet(alphanumeric, 20)();
