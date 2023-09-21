1201e15aded6de0f035bf84c567c128c526999fc
Lucky3028 <me@mail.cap-l.cc>
2023-09-22 00:01:43 +0900

    feat: each trigger must have its own id

--- apps/auto-run-ac-api/src/db/schema.ts
+++ apps/auto-run-ac-api/src/db/schema.ts
@@ -1,17 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { sqliteTable, text, integer, unique } from 'drizzle-orm/sqlite-core';
 
-const tempColumn = integer('trigger_temp').notNull();
-const operationModeColumn = text('operation_mode', { enum: ['HOT', 'COOL'] }).notNull();
-const settingsTempColumn = integer('settings_temp').notNull();
+const id = text('id').primaryKey();
+const triggerTemp = integer('trigger_temp').notNull();
+const operationMode = text('operation_mode', { enum: ['HOT', 'COOL'] }).notNull();
+const settingsTemp = integer('settings_temp').notNull();
 
 export const defaultTriggersTable = sqliteTable(
   'default_triggers',
   {
+    id,
     triggerTime: integer('trigger_time', { mode: 'timestamp' }).notNull(),
-    triggerTemp: tempColumn,
-    operationMode: operationModeColumn,
-    settingsTemp: settingsTempColumn,
+    triggerTemp,
+    operationMode,
+    settingsTemp,
   },
   (table) => ({
     unq: unique().on(table.triggerTime, table.operationMode),
@@ -21,10 +23,11 @@ export const defaultTriggersTable = sqliteTable(
 export const dateTriggersTable = sqliteTable(
   'date_triggers',
   {
+    id,
     triggerDateTime: integer('trigger_date_time', { mode: 'timestamp' }).notNull(),
-    triggerTemp: tempColumn,
-    operationMode: operationModeColumn,
-    settingsTemp: settingsTempColumn,
+    triggerTemp,
+    operationMode,
+    settingsTemp,
   },
   (table) => ({
     unq: unique().on(table.triggerDateTime, table.operationMode),
