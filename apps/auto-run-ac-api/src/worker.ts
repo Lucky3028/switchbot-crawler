c554651ccf94743370ce32fc866b14cbfb38aff6
Lucky3028 <me@mail.cap-l.cc>
2023-09-25 23:36:03 +0900

    feat: add /docs endpoint

--- apps/auto-run-ac-api/src/worker.ts
+++ apps/auto-run-ac-api/src/worker.ts
@@ -1,11 +1,31 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { drizzle } from 'drizzle-orm/d1';
+import { cors } from 'hono/cors';
 import { defaultTriggersApi, triggersApi } from './api';
 import type { Env, Variables } from './model';
 
 const app = new OpenAPIHono<{ Bindings: Env; Variables: Variables }>();
 
 app.get('/', (c) => c.text('Hello Hono!'));
+
+// TODO: fix CORS origin
+app.use(
+  '*',
+  cors({
+    origin: ['http://localhost:8080'],
+  }),
+);
+
+app.openAPIRegistry.registerComponent('securitySchemes', 'basicAuth', { type: 'http', scheme: 'basic' });
+
+app.doc31('/docs', {
+  openapi: '3.1.0',
+  info: {
+    version: '1.0.0',
+    title: 'auto-run-api',
+  },
+  security: [{ basicAuth: [] }],
+});
 
 app.use('*', async (c, next) => {
   c.set('db', drizzle(c.env.triggers));
