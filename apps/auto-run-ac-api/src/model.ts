26d379acc8411dd7595db87ed92e86026988aa71
Lucky3028 <me@mail.cap-l.cc>
2023-09-27 23:29:04 +0900

    fix: replace zod with hono-zod

--- apps/auto-run-ac-api/src/model.ts
+++ apps/auto-run-ac-api/src/model.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import { z } from '@hono/zod-openapi';
 import type { DrizzleD1Database } from 'drizzle-orm/d1';
 import { parseDecimalInt } from './lib';
 
@@ -36,7 +36,7 @@ const timeSchema = z.object({
   minute: z.number().int().nonnegative().multipleOf(5).max(55),
 });
 
-const acSettingsSchema = z.object({
+export const acSettingsSchema = z.object({
   mode: z.enum(['HOT', 'COOL'], { invalid_type_error: "Operation mode must be 'HOT' or 'COOL'." }),
   temp: z
     .number({ invalid_type_error: 'Temp must be integer value' })
