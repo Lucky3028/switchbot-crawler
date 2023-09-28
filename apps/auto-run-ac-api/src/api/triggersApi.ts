62a35366f9d9d8a142cbe9c1e3e69874872bb5c4
Lucky3028 <me@mail.cap-l.cc>
2023-09-28 23:49:29 +0900

    feat: add /triggers

--- apps/auto-run-ac-api/src/api/triggersApi.ts
+++ apps/auto-run-ac-api/src/api/triggersApi.ts
@@ -1,93 +1,267 @@
-import { dateSchema, triggersSchema, type Env, type Variables } from '@/model';
-import { OpenAPIHono } from '@hono/zod-openapi';
-import { zValidator } from '@hono/zod-validator';
+import { triggersSchema, type Env, type Variables, triggerSchema, acSettingsSchema } from '@/model';
+import { OpenAPIHono, createRoute, z } from '@hono/zod-openapi';
 import type { SharedEnv } from 'cloudflare-env';
+import { dateTriggersTable as table } from '@/db/schema';
+import { emptyJsonT, getUrl, nanoid } from '@/lib';
+import { eq } from 'drizzle-orm';
 
 const app = new OpenAPIHono<{ Bindings: SharedEnv & Env; Variables: Variables }>();
 
 export const triggersApi = app
-  .get(
-    '/:year/:month/:day',
-    zValidator(
-      'param',
-      dateSchema,
-      // @ts-ignore TS7030
-      // eslint-disable-next-line consistent-return
-      (result, c) => {
-        if (!result.success) {
-          return c.jsonT({ success: false, messages: result.error.issues.map((i) => i.message) }, 400);
-        }
+  .openapi(
+    createRoute({
+      responses: {
+        200: {
+          description: 'OK',
+          content: {
+            'application/json': {
+              schema: z.object({
+                success: z.boolean(),
+                data: z.object({
+                  counts: z.number().int().nonnegative(),
+                  triggers: triggersSchema,
+                }),
+              }),
+            },
+          },
+        },
       },
-    ),
-    async (c) => {
-      const { year, month, day } = c.req.valid('param');
-      const value = await c.env.TRIGGERS.get(`triggers/${year}/${month}/${day}`, { type: 'json' });
-      if (!value) {
-        const values = await c.env.TRIGGERS.get('defaultTriggers', { type: 'json' });
-        if (!values) {
-          return c.jsonT({ success: true, data: { counts: 0, triggers: [] } });
-        }
-
-        const triggers = triggersSchema.parse(values);
-
-        return c.jsonT({ success: true, data: { triggers, counts: triggers.length } });
-      }
-
-      const triggers = triggersSchema.parse(value);
-
-      return c.jsonT({ success: true, data: { triggers, counts: triggers.length } });
-    },
-  )
-  .put(
-    '/:year/:month/:day',
-    zValidator(
-      'param',
-      dateSchema,
-      // @ts-ignore TS7030
-      // eslint-disable-next-line consistent-return
-      (result, c) => {
-        if (!result.success) {
-          return c.jsonT({ success: false, messages: result.error.issues.map((i) => i.message) }, 400);
-        }
-      },
-    ),
-    // @ts-ignore TS7030
-    // eslint-disable-next-line consistent-return
-    zValidator('json', triggersSchema, (result, c) => {
-      if (c.req.raw.headers.get('Content-Type') !== 'application/json') {
-        return c.jsonT({ success: false, messages: ['Content-Type must be "application/json"'] }, 415);
-      }
-      if (!result.success) {
-        return c.jsonT({ success: false, messages: result.error.issues.map((i) => i.message) }, 400);
-      }
+      method: 'get',
+      path: '/',
     }),
     async (c) => {
-      const { year, month, day } = c.req.valid('param');
-      const contents = c.req.valid('json');
+      const values = await c.get('db').select().from(table);
+      const triggers = values.map(({ triggerDateTime, triggerTemp, operationMode, settingsTemp, ...v }) => ({
+        ...v,
+        temp: triggerTemp,
+        ac: { mode: operationMode, temp: settingsTemp },
+        dateTime: triggerDateTime,
+      }));
+      const response = { triggers, counts: triggers.length };
 
-      c.executionCtx.waitUntil(c.env.TRIGGERS.put(`triggers/${year}/${month}/${day}`, JSON.stringify(contents)));
-
-      return c.body(null, 204);
+      return c.jsonT({ success: true, data: response });
     },
   )
-  .delete(
-    '/:year/:month/:day',
-    zValidator(
-      'param',
-      dateSchema,
-      // @ts-ignore TS7030
-      // eslint-disable-next-line consistent-return
-      (result, c) => {
-        if (!result.success) {
-          return c.jsonT({ success: false, messages: result.error.issues.map((i) => i.message) }, 400);
-        }
+  .openapi(
+    createRoute({
+      request: {
+        body: {
+          content: {
+            'application/json': {
+              schema: triggerSchema.omit({ id: true }),
+            },
+          },
+        },
       },
-    ),
+      responses: {
+        201: {
+          description: 'Trigger is created',
+          content: {
+            'application/json': {
+              schema: z.object({ success: z.boolean(), data: z.object({ trigger: triggerSchema }) }),
+            },
+          },
+        },
+      },
+      method: 'post',
+      path: '/',
+    }),
     async (c) => {
-      const { year, month, day } = c.req.valid('param');
+      const contents = c.req.valid('json');
 
-      c.executionCtx.waitUntil(c.env.TRIGGERS.delete(`triggers/${year}/${month}/${day}`));
+      const id = nanoid();
+      await c
+        .get('db')
+        .insert(table)
+        .values({
+          id,
+          triggerTemp: contents.temp,
+          triggerDateTime: contents.dateTime,
+          settingsTemp: contents.ac.temp,
+          operationMode: contents.ac.mode,
+        })
+        .onConflictDoNothing();
+      const trigger = { ...contents, id };
+      const response = { success: true, data: { trigger } };
 
-      return c.body(null, 204);
+      return c.jsonT(response, 201, { Location: `${getUrl(c.req)}/${id}` });
+    },
+  )
+  .openapi(
+    createRoute({
+      request: {
+        params: z.object({ id: z.string() }),
+      },
+      responses: {
+        200: {
+          description: 'The trigger is found',
+          content: {
+            'application/json': {
+              schema: z.object({ success: z.boolean(), data: z.object({ trigger: triggerSchema }) }),
+            },
+          },
+        },
+        404: {
+          description: 'The trigger is not found',
+          content: {
+            'application/json': {
+              schema: z.object({
+                success: z.boolean(),
+              }),
+            },
+          },
+        },
+      },
+      method: 'get',
+      path: '/{id}',
+    }),
+    async (c) => {
+      const { id } = c.req.valid('param');
+      const [value] = await c.get('db').select().from(table).where(eq(table.id, id));
+
+      if (!value) {
+        return c.jsonT({ success: false }, 404);
+      }
+
+      const trigger = {
+        ...value,
+        temp: value.triggerTemp,
+        ac: { mode: value.operationMode, temp: value.settingsTemp },
+        dateTime: value.triggerDateTime,
+      };
+
+      return c.jsonT({ success: true, data: { trigger } });
+    },
+  )
+  .openapi(
+    createRoute({
+      request: {
+        params: z.object({ id: z.string() }),
+        body: {
+          content: {
+            'application/json': {
+              schema: triggerSchema.pick({ dateTime: true }),
+            },
+          },
+        },
+      },
+      responses: {
+        204: {
+          description: 'The trigger is modified',
+        },
+      },
+      method: 'put',
+      path: '/{id}/dateTime',
+    }),
+    async (c) => {
+      const { id } = c.req.valid('param');
+      const { dateTime } = c.req.valid('json');
+      await c.get('db').update(table).set({ triggerDateTime: dateTime }).where(eq(table.id, id));
+
+      return emptyJsonT(c, 204);
+    },
+  )
+  .openapi(
+    createRoute({
+      request: {
+        params: z.object({ id: z.string() }),
+        body: {
+          content: {
+            'application/json': {
+              schema: triggerSchema.pick({ temp: true }),
+            },
+          },
+        },
+      },
+      responses: {
+        204: {
+          description: 'The trigger is modified',
+        },
+      },
+      method: 'put',
+      path: '/{id}/temp',
+    }),
+    async (c) => {
+      const { id } = c.req.valid('param');
+      const { temp } = c.req.valid('json');
+      await c.get('db').update(table).set({ triggerTemp: temp }).where(eq(table.id, id));
+
+      return emptyJsonT(c, 204);
+    },
+  )
+  .openapi(
+    createRoute({
+      request: {
+        params: z.object({ id: z.string() }),
+        body: {
+          content: {
+            'application/json': {
+              schema: acSettingsSchema.pick({ mode: true }),
+            },
+          },
+        },
+      },
+      responses: {
+        204: {
+          description: 'The trigger is modified',
+        },
+      },
+      method: 'put',
+      path: '/{id}/acMode',
+    }),
+    async (c) => {
+      const { id } = c.req.valid('param');
+      const { mode } = c.req.valid('json');
+      await c.get('db').update(table).set({ operationMode: mode }).where(eq(table.id, id));
+
+      return emptyJsonT(c, 204);
+    },
+  )
+  .openapi(
+    createRoute({
+      request: {
+        params: z.object({ id: z.string() }),
+        body: {
+          content: {
+            'application/json': {
+              schema: acSettingsSchema.pick({ temp: true }),
+            },
+          },
+        },
+      },
+      responses: {
+        204: {
+          description: 'The trigger is modified',
+        },
+      },
+      method: 'put',
+      path: '/{id}/acTemp',
+    }),
+    async (c) => {
+      const { id } = c.req.valid('param');
+      const { temp } = c.req.valid('json');
+      await c.get('db').update(table).set({ settingsTemp: temp }).where(eq(table.id, id));
+
+      return emptyJsonT(c, 204);
+    },
+  )
+  .openapi(
+    createRoute({
+      request: {
+        params: z.object({ id: z.string() }),
+      },
+      responses: {
+        204: {
+          description: 'The trigger is deleted',
+        },
+      },
+      method: 'delete',
+      path: '/{id}',
+    }),
+    async (c) => {
+      const { id } = c.req.valid('param');
+      await c.get('db').delete(table).where(eq(table.id, id));
+
+      return emptyJsonT(c, 204);
     },
   );
