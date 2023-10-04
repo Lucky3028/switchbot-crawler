81bd1357d876f8ee321910f08065d7f6f3d15ad7
Lucky3028 <me@mail.cap-l.cc>
2023-10-04 11:46:49 +0900

    feat: each OpenAPI value should have its example value

--- apps/auto-run-ac-api/src/model.ts
+++ apps/auto-run-ac-api/src/model.ts
@@ -8,50 +8,63 @@ export type Env = {
 
 export type Variables = { db: DrizzleD1Database };
 
-export const dateSchema = z.object({
-  year: z
-    .string()
-    .transform(parseDecimalInt)
-    .refine((v) => Number.isInteger(v), { message: 'Year must be an integer' })
-    .refine((n) => n >= 2000, { message: 'Year must be equal or greater than 2000' }),
-  month: z
-    .string()
-    .transform(parseDecimalInt)
-    .refine((v) => Number.isInteger(v), { message: 'Month must be an integer' })
-    .refine((n) => n >= 1 && n <= 12, { message: 'Month must be between 1 and 12' }),
-  day: z
-    .string()
-    .transform(parseDecimalInt)
-    .refine((v) => Number.isInteger(v), { message: 'Day must be an integer' })
-    .refine((n) => n >= 1 && n <= 31, { message: 'Month must be between 1 and 31' }),
-});
+export const dateSchema = z
+  .object({
+    year: z
+      .string()
+      .transform(parseDecimalInt)
+      .refine((v) => Number.isInteger(v), { message: 'Year must be an integer' })
+      .refine((n) => n >= 2000, { message: 'Year must be equal or greater than 2000' }),
+    month: z
+      .string()
+      .transform(parseDecimalInt)
+      .refine((v) => Number.isInteger(v), { message: 'Month must be an integer' })
+      .refine((n) => n >= 1 && n <= 12, { message: 'Month must be between 1 and 12' }),
+    day: z
+      .string()
+      .transform(parseDecimalInt)
+      .refine((v) => Number.isInteger(v), { message: 'Day must be an integer' })
+      .refine((n) => n >= 1 && n <= 31, { message: 'Month must be between 1 and 31' }),
+  })
+  .openapi({
+    example: {
+      year: 2020,
+      month: 1,
+      day: 15,
+    },
+  });
 
-const timeSchema = z.object({
-  hour: z
-    .number({ invalid_type_error: 'Hour must be integer value' })
-    .int({ message: 'Hour must be integer value' })
-    .nonnegative({ message: 'Hour must be non-negative value' })
-    .max(23, { message: 'Hour must be equal or less than 23' }),
-  minute: z.number().int().nonnegative().multipleOf(5).max(55),
-});
+const timeSchema = z
+  .object({
+    hour: z
+      .number({ invalid_type_error: 'Hour must be integer value' })
+      .int({ message: 'Hour must be integer value' })
+      .nonnegative({ message: 'Hour must be non-negative value' })
+      .max(23, { message: 'Hour must be equal or less than 23' }),
+    minute: z.number().int().nonnegative().multipleOf(5).max(55),
+  })
+  .openapi({ example: { hour: 12, minute: 30 } });
 
-export const acSettingsSchema = z.object({
-  mode: z.enum(['HOT', 'COOL'], { invalid_type_error: "Operation mode must be 'HOT' or 'COOL'." }),
-  temp: z
-    .number({ invalid_type_error: 'Temp must be integer value' })
-    .int({ message: 'Temp must be integer value' })
-    .min(16, { message: 'Temp must be equal or greater than 16' })
-    .max(30, { message: 'Hour must be equal or less than 30' }),
-});
+export const acSettingsSchema = z
+  .object({
+    mode: z.enum(['HOT', 'COOL'], { invalid_type_error: "Operation mode must be 'HOT' or 'COOL'." }),
+    temp: z
+      .number({ invalid_type_error: 'Temp must be integer value' })
+      .int({ message: 'Temp must be integer value' })
+      .min(16, { message: 'Temp must be equal or greater than 16' })
+      .max(30, { message: 'Hour must be equal or less than 30' }),
+  })
+  .openapi({ example: { mode: 'COOL', temp: 25 } });
 
 export const triggerSchema = z.object({
-  id: z.string().nonempty({ message: "Trigger's ID must be nonempty" }),
-  dateTime: z.date(),
+  id: z.string().nonempty({ message: "Trigger's ID must be nonempty" }).openapi({ example: 'W5vQ3TMBUFvaRLRf88VH' }),
+  dateTime: z.date().openapi({ example: '2020-01-15T09:30:00Z' }),
   temp: z
     .number({ invalid_type_error: 'Temp must be integer value' })
     .int({ message: 'Temp must be integer value' })
     .nonnegative({ message: 'Temp must be non-negative value' })
-    .max(40, { message: 'Temp must be equal or less than 40' }),
+    .max(40, { message: 'Temp must be equal or less than 40' })
+    .openapi({ example: 35 }),
   ac: acSettingsSchema,
 });
 
